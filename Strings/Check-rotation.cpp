a0c3e684a5ef812b2a6885de72c83d81e4899727
kgxkaran <karangera025@gmail.com>
2025-07-26 20:48:47 +0530

    Update Check-rotation.cpp

--- Strings/Check-rotation.cpp
+++ Strings/Check-rotation.cpp
@@ -32,3 +32,25 @@ public:
         return s.find(goal) != string::npos;
     }
 };
+
+In C++, string::npos is a special constant used in operations on strings (like find, rfind, etc.) to represent:
+
+✅ "No Position" or ❗ "Not Found"
+
+Q) return index of the substring if found !!
+
+  #include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s = "karan";
+    string s2 = "narak";
+    string name = s + s2;
+    size_t pos = name.find(s2);
+    
+    if(pos == string::npos){
+        cout << "Not found ";
+    }else{
+        cout << "Found at " << pos;
+    }
+}
