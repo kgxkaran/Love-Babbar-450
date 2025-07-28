95b1f107ed60894266f48fc26abf334ea9ea64eb
kgxkaran <karangera025@gmail.com>
2025-07-29 01:06:05 +0530

    Update MergeSortedArray.cpp

--- Arrays/MergeSortedArray.cpp
+++ Arrays/MergeSortedArray.cpp
@@ -13,8 +13,6 @@ public:
     }
 };
 
-
-
 Approach 2 :- 
 
   Time Complexity: O(m + n)
@@ -42,6 +40,29 @@ public:
     }
 };
 
-Approach 2:- 
+Approach 3:- Optimal 
 
+  class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+
+        while(i >= 0 && j >= 0){
+            if(nums1[i] > nums2[j]){
+                nums1[k--] = nums1[i--];
+            }else{
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        while(j >= 0){
+            nums1[k--] = nums2[j--];
+        }
+        
+    }
+};
+
+  
   
