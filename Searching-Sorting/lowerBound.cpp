85509b4273f4d49f776a62599afdf560549ea210
kgxkaran <karangera025@gmail.com>
2025-08-01 13:32:53 +0530

    Create lowerBound.cpp

---
+++ Searching-Sorting/lowerBound.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int lowerBound(vector<int>&arr,int key){
+    int mini = -1;
+    int low = 0;
+    int high = arr.size()-1;
+    while(low <= high){
+        int mid = low + (high-low)/2;
+        if(arr[mid] >= key){
+            mini = mid;
+            high = mid-1;
+        }else{
+            low = mid+1;
+        }
+    }
+    return mini;
+}
+int main(){
+    vector<int>arr = {1,1,2,2,3,3,3,5,6};
+    int k = 4;
+    int ans = lowerBound(arr,k);
+    cout << ans;
+}
