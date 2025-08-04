d00066a689accd3a38497bfb45a97da6f940b677
kgxkaran <karangera025@gmail.com>
2025-08-04 18:03:37 +0530

    Create sqrt.cpp

---
+++ Searching-Sorting/sqrt.cpp
@@ -0,0 +1,25 @@
+https://leetcode.com/problems/sqrtx/
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 0 || x == 1) return x;
+        int low = 0;
+        int high = x/2;
+        int ans = 0;
+        while(low <= high){
+            int mid = low+(high-low)/2;
+            long square = 1LL*mid*mid;
+            if(square == x){
+                return mid;
+            }else if(square > x){
+                high = mid-1;  
+            }else{
+                ans = mid;
+                low = mid+1;
+            }
+        }
+
+        return ans;
+    }
+};
