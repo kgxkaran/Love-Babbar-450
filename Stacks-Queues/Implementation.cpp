728148a5ed58c56c4e7b79bf44e73b8e3248e116
kgxkaran <karangera025@gmail.com>
2025-08-04 23:51:11 +0530

    Create Implementation.cpp

---
+++ Stacks-Queues/Implementation.cpp
@@ -0,0 +1,51 @@
+1. Implementing Stack using arrays :- 
+
+  #include<bits/stdc++.h>
+using namespace std;
+class Stack{
+private:
+       int arr[100];
+       int top;
+public:
+      Stack(){
+          top = -1;
+      };
+      void push(int x){
+          if(top >= 99){
+              cout << "Stack Overflow !!"<<endl;
+              return;
+          }
+          arr[++top] = x;
+      }
+      void pop(){
+          if(top == -1){
+              cout << "Stack Underflow !!" << endl;
+              return;
+          }
+          top--;
+      }
+      int peek(){
+          if(top == -1){
+              cout << "Stack is empty !! " <<endl;
+          }
+          return arr[top];
+      }
+      int size(){
+          return top+1;
+      }
+      bool isEmpty(){
+          if(top == -1) return true;
+          return false;
+      }
+};
+
+int main(){
+    Stack s;
+    s.push(4);
+    s.push(6);
+    s.push(5);
+   cout << s.peek() << endl;
+   cout << s.isEmpty() << endl;
+   cout << s.size() << endl;
+    
+}
