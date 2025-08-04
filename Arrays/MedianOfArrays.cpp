cc7a404708e18e725d6a3ee1f8b0ff6cde82619a
kgxkaran <karangera025@gmail.com>
2025-08-04 13:21:47 +0530

    Update MedianOfArrays.cpp

--- Arrays/MedianOfArrays.cpp
+++ Arrays/MedianOfArrays.cpp
@@ -21,3 +21,27 @@ class Solution {
 Q2) Median of 2 sorted arrays with EQUAL SIZES (Actual question)
 https://www.geeksforgeeks.org/problems/median-of-2-sorted-arrays-of-same-size/1
 
+Naive Approach :- 
+
+  class Solution {
+  public:
+    double medianOf2(vector<int>& a, vector<int>& b) {
+        // Your code goes here
+        vector<int>store;
+        for(int i = 0; i < a.size(); i++){
+            store.push_back(a[i]);
+        }
+        for(int i = 0; i < b.size(); i++){
+            store.push_back(b[i]);
+        }
+        sort(store.begin() , store.end());
+        int low = 0;
+        int high = store.size()-1;
+        int mid = low+(high-low)/2;
+        
+        double ans = (store[mid] + store[mid+1])/2.0;
+        
+        return ans;
+    }
+};
+
