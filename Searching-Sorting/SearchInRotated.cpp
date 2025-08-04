29ee938ef8778c10bd9f4a3c97670c8a647d00e1
kgxkaran <karangera025@gmail.com>
2025-08-04 17:01:58 +0530

    Update SearchInRotated.cpp

--- Searching-Sorting/SearchInRotated.cpp
+++ Searching-Sorting/SearchInRotated.cpp
@@ -51,3 +51,34 @@ public:
         return left;
     }
 };
+
+Approach 2 :- Best Approach O(LogN)
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size()-1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target) return mid;
+
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && nums[mid] > target){
+                    high = mid-1;
+                }else{
+                    low = mid+1;
+                }
+            }else{
+                if(nums[mid] < target && target <= nums[high]){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+  
