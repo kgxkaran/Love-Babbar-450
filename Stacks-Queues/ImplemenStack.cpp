d879eac7331da0e4f9563c4799c984dbed8ba559
kgxkaran <karangera025@gmail.com>
2025-08-05 12:25:58 +0530

    Update ImplemenStack.cpp

--- Stacks-Queues/ImplemenStack.cpp
+++ Stacks-Queues/ImplemenStack.cpp
@@ -46,8 +46,86 @@ int main(){
     s.push(5);
    cout << s.peek() << endl;
    cout << s.isEmpty() << endl;
-   cout << s.size() << endl;
-    
+   cout << s.size() << endl;    
+}
+
+
+
+2. Implement Stack using LinkedList :-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node*next;
+};
+class Stack{
+private:
+      Node*top;
+public:
+      Stack(){
+          top = nullptr;
+      };
+      
+      void push(int x){
+          Node* newNode = new Node();
+          newNode->data = x;
+          newNode->next = top;
+          top = newNode;
+      }
+      void pop(){
+          if(top == nullptr){
+              cout << "Stack is empty !!" <<endl;
+              return;
+          }
+          Node*temp = top;
+          top = top->next;
+          delete temp;
+      }
+      int peek(){
+          if(top == nullptr){
+              cout << "Stack is empty !!" <<endl;
+              return -1;
+          }
+          return top->data;
+      }
+      bool isEmpty(){
+          return top == nullptr;
+      }
+      void display(){
+          if(top == nullptr){
+              cout << "Nothing to display"<<endl;
+              return;
+          }
+          Node*temp = top;
+          while(temp!=nullptr){
+              cout << temp->data << endl;
+              temp = temp->next;
+          }
+      }
+      int size(){
+          if(top == nullptr) return 0;
+          int count = 0;
+          Node* temp = top;
+          while(temp!=nullptr){
+              count++;
+              temp = temp->next;
+          }
+          return count;
+      }
+};
+
+int main(){
+    Stack s;
+    s.push(3);
+    s.push(15);
+    cout << "Initial size " << s.size() << endl;
+    s.push(6);
+    s.push(4);
+    cout << "Updated size is " << s.size() << endl;
+    cout << "Top element is " << s.peek() << endl;
+    s.display();
 }
 
 
