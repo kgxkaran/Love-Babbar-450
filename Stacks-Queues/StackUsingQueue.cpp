dc7ea5d01b9331f839a308f565c00d138bfd3de0
kgxkaran <karangera025@gmail.com>
2025-08-06 01:05:35 +0530

    Update StackUsingQueue.cpp

--- Stacks-Queues/StackUsingQueue.cpp
+++ Stacks-Queues/StackUsingQueue.cpp
@@ -32,3 +32,38 @@ public:
         return que1.empty();        
     }
 };
+
+
+2. Using only 1 Queue :- 
+
+  class MyStack {
+public:
+    queue<int> que;
+
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        que.push(x);
+        for(int i = 0; i < que.size() - 1; i++){
+            que.push(que.front());
+            que.pop();
+        }
+
+    }
+    
+    int pop() {
+        int result = que.front();
+        que.pop();
+        return result;
+    }
+    
+    int top() {
+        return que.front(); 
+    }
+    
+    bool empty() {
+        return que.empty();        
+    }
+};
